92db41b03d696c1ffb14944648b4255a3710fe31
Vimchenko <e.vimchenko@gmail.com>
2021-03-03 13:55:24 +0200

    Small work

--- send.py
+++ send.py
@@ -1,32 +1,31 @@
-from io import BytesIO
-
-from reportlab.pdfgen import canvas
-from django.http import HttpResponse
-import email, smtplib, ssl
-from email import encoders
-from email.mime.base import MIMEBase
+import smtplib
 from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
-from email.header import Header
 
-sender_mail = 'faefafae@umpa.com'
-sender_password = 'qwerty'
+# настройка ключевых переменных: содержание текста, отправитель (мы)
+sender_mail = 'myMail@gmail.com'
+sender_password = 'PASSWORD_EXAMPLE'
+subject = 'MESSAGE!'
+mail_body_text = 'What is up?'
+# получатель
+target_mail = 'hisherMail@gmail.com'
 
-target_mail = 'qwdqwdq@klukva.com'
-
-subject = 'STEP'
-msg = MIMEMultipart()
+#  email Head: отправитель, получатель, объект
+msg = MIMEMultipart('good')
 msg['From'] = sender_mail
 msg['To'] = target_mail
-msg.add_header('reply_to', sender_mail)
+msg['Subject'] = subject
+msg.add_header('reply-to', sender_mail)
+# сессия для отправки почтой
+mail_sender = smtplib.SMTP('smtp.gmail.com', 587)
+mail_sender.starttls()
+mail_sender.login(sender_mail, sender_password)
+mail_sender.sendmail(sender_mail, target_mail, msg.as_string())
+mail_sender.quit()
+# уведомить нас об успешной отправке
+print('OK!')
 
-mailsender = smtplib.SMTP_SSL('smtp.gmail.com', 600)
-#mailsender.starttls()
-mailsender.login(sender_mail, sender_password)
-mail_subject = 'Hi. How are You?'
-mail_body_text = 'This is the End'
-mail_body_html = '<html><head><body></body><a href="bonk.com"</a></head></html>'
-msg = MIMEText(mail_body_html, 'html', 'utf-8')
-msg['Subject'] = Header(mail_subject, 'utf-8')
-mailsender.sendmail(sender_mail, target_mail, msg.as_string())
-mailsender.quit()
+
+''' ПРИМЕЧАНИЕ: 
+для корректной работы авторизации через библиотеку smtplib, 
+вам нужно разрешить непроверенным сообщениям доступ к почте. Ссылка: 
+https://myaccount.google.com/lesssecureapps?pli=1&rapt=AEjHL4PuXAqdqYZaj1mOzVvrWNSDXDt5yfqFdwWHBvERSOkK0L67GLNj8JOlDwAJFSOkxwYQ9MLy3u0TTMG8UUq-Ifdck7-N1A '''
